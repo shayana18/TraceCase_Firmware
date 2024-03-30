b003b2cc19e461157ce9a0af1a88130821ad5b71
zaydk13 <zaydkhalidi@gmail.com>
2024-03-30 13:27:17 -0700

    Update UltrasonicTest.ino

--- Arduino_Control/UltrasonicTest/UltrasonicTest.ino
+++ Arduino_Control/UltrasonicTest/UltrasonicTest.ino
@@ -1,13 +1,11 @@
-
-
-
 /*
-  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
-
-  by Dejan Nedelkovski,
-  www.HowToMechatronics.com
-
+Simple tests for the ultrasonic and its ensuing controls
+- Zayd Khalidi 14/03/2024
 */
+
+#include <math.h>
+
+
 // defines pins numbers
 const int ENA = 3;
 const int IN1 = 4;
@@ -22,19 +20,36 @@ const int echoPinL = 11;
 const int trigPinR = 12;
 const int echoPinR = 13;
 
-// defines variables
+// PID constants
+const double Kp = 0.5;
+const double Ki = 0;
+const double Kd = 0;
+const int Set_Point = 100;
+const double windupGuard = 255;
+const float iMax = 255;
+const float iMin = 0;
+
+// general variables
 long durationL;
-int distanceL;
+double distanceL;
 long durationR;
-int distanceR;
-int obstacle[2];
+double distanceR;
+double obstacle[2];
+double distanceU;
+
+// PID variables
+float d_Temp = 0;
+float i_Temp = 0;
+float PWM_Temp = 166;
+float PWM_Duty = 166;
+
 void setup() {
-  pinMode(ENA, OUTPUT);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
-  pinMode(ENB, OUTPUT);
+  pinMode(ENA, OUTPUT); // Right Motor PWNM
+  pinMode(IN1, OUTPUT); // Right Motor Forwards
+  pinMode(IN2, OUTPUT); // Right Motor Backwards
+  pinMode(IN3, OUTPUT); // Left Motor Forwards
+  pinMode(IN4, OUTPUT); // Left Motor Backwards
+  pinMode(ENB, OUTPUT); // Left Motor PWM
 
   pinMode(trigPinL, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPinL, INPUT); // Sets the echoPin as an Input
@@ -45,28 +60,28 @@ void setup() {
 }
 void loop() {
   // Clears the trigPin
-  digitalWrite(trigPinL, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPinL, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinL, LOW);
+  digitalWrite(trigPinL, LOW); // 3ums
+  delayMicroseconds(2); // 2ums
+  digitalWrite(trigPinL, HIGH); // 3ums
+  delayMicroseconds(10); // 10ums
+  digitalWrite(trigPinL, LOW); //3ums
   // Reads the echoPin, returns the sound wave travel time in microseconds
   durationL = pulseIn(echoPinL, HIGH);
   // Calculating the distance
-  distanceL = durationL * 0.034 / 2;
+  distanceL = durationL * 0.034 / 2; // ~3ums
 
   // Clears the trigPin
-  digitalWrite(trigPinR, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPinR, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPinR, LOW);
+  digitalWrite(trigPinR, LOW); // 3ums
+  delayMicroseconds(2);  // 2ums
+  digitalWrite(trigPinR, HIGH); // 3ums
+  delayMicroseconds(10); // 10ums
+  digitalWrite(trigPinR, LOW); // 3ums
   // Reads the echoPin, returns the sound wave travel time in microseconds
   durationR = pulseIn(echoPinR, HIGH);
   // Calculating the distance
-  distanceR = durationR * 0.034 / 2;
+  distanceR = durationR * 0.034 / 2; // ~3ums
+
+// Total delay from both R&L = ~50ums
 
 //  // Prints the distance on the Serial Monitor
 //  Serial.print("Distance Left: ");
@@ -78,44 +93,57 @@ void loop() {
 
   obstacle[0] = distanceL;
   obstacle[1] = distanceR;
-  Serial.println(obstacle[1]);
 
+  Serial.print("Distance: ");
+  Serial.println(obstacle[1]);
+  Serial.print("PID: ");
+  Serial.println(PID(obstacle[1]));
+
+  int distancePWM = 285*(1 - exp(PID(distanceU))/85) - 15.5; // Normalize user distance to a speed
+  int PWMdiff = 255/250 * (distanceR - distanceL);
   
-// 
-//  if(distance < 80)
-//  {
-//    digitalWrite(IN1, HIGH);
-//    digitalWrite(IN2, LOW);
-//    digitalWrite(IN3, HIGH);
-//    digitalWrite(IN4, LOW);
-//    delay(5);
-//  }
-//  else if(distance > 120 && distance < 1000)
-//  {
-//    digitalWrite(IN1, LOW);
-//    digitalWrite(IN2, HIGH);
-//    digitalWrite(IN3, LOW);
-//    digitalWrite(IN4, HIGH);
-//    delay(5);
-//  }
-//  else
-//  {
-//    digitalWrite(IN1, LOW);
-//    digitalWrite(IN2, LOW);
-//    digitalWrite(IN3, LOW);
-//    digitalWrite(IN4, LOW);
-//    delay(5);
-//  }
  
 }
 
-void PID() {
+double PID(double newVal) {
 
-  double Kp = 1;
-  double Ki = 1;
-  double Kd = 1;
+  float errVal;
+  float P;
+  float I;
+  float D;
 
+  if (newVal > 800)
+  newVal = PWM_Duty;
   
+  // Something to try: for an array of read vals, we can keep each and decay them according to exp(-4*n/(N-1))
+  errVal = (Set_Point - newVal);
+  P = -Kp*errVal;
+  
+  i_Temp += errVal;
+  if (i_Temp > windupGuard)
+    i_Temp = windupGuard;
+  else if (i_Temp < -windupGuard)
+    i_Temp = -windupGuard;
+    
 
+  if (i_Temp > iMax)
+    i_Temp = iMax;
+  else if (i_Temp < iMin)
+    i_Temp = iMin;
+
+  I = Ki*i_Temp;
+
+  D = Kd*(d_Temp - errVal);
+  d_Temp = errVal;
+
+  PWM_Duty = PWM_Temp - (P + I + D);
+//  if (PWM_Duty > 255)
+//    PWM_Duty = 255;
+//  if (PWM_Duty < 5)
+//    PWM_Duty = 5;
+
+  PWM_Temp = PWM_Duty;
+  
+  return PWM_Duty;
 
 }
